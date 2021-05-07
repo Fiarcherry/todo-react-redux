fa1da3868125f6ec821c9af5edc58c97c7ec37e4
fiarcherry <fiarchersschannel@gmail.com>
2021-05-07 19:31:46 +0300

    Динамический список и массив начальных данных

--- src/index.js
+++ src/index.js
@@ -7,12 +7,18 @@ import ItemStatusFilter from './components/item-status-filter'
 import TodoList from './components/todo-list'
 
 const App = () => {
+  const todoData = [
+    { id: 0, label: 'Drink Coffee', important: false },
+    { id: 1, label: 'Make Awesome App', important: true },
+    { id: 2, label: 'Have a lunch', important: false },
+  ]
+
   return (
     <div>
       <AppHeader />
       <SearchPanel />
-      <TodoList />
       <ItemStatusFilter />
+      <TodoList todos={todoData} />
     </div>
   )
 }
