74661fccc4e649be6e3985a92380d270a828285b
fiarcherry <fiarchersschannel@gmail.com>
2021-05-24 11:55:34 +0300

    Разделитель отрисовывается как стиль, а не отдельный компонент

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -9,15 +9,16 @@ import ListElement from '../common/ListElement'
 const TodoList = ({ todos, onDeleted, onToggleImportant, onToggleDone }) => {
   const elements = todos.map((item, index) => {
     const { id, ...itemProps } = item
+    const last = todos.length - 1 === index
+
     return (
-      <ListElement key={id}>
+      <ListElement key={id} last={last}>
         <TodoListItem
           {...itemProps}
           onDeleted={() => onDeleted(id)}
           onToggleImportant={() => onToggleImportant(id)}
           onToggleDone={() => onToggleDone(id)}
         />
-        {todos.length - 1 === index ? '' : <Divider />}
       </ListElement>
     )
   })
